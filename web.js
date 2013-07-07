24f486adc0b4d184eec0cd964d7d9c4221145f14
Sergio Gonzalez <sgonzale11@yahoo.com>
2013-07-07 14:09:51 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = fs.readFileSync('./index.html');  
+  response.send(buffer.toString('utf-8'));
 });
+// fs.readFileSync(filename, [options])
+//buf.toString([encoding], [start], [end])
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
